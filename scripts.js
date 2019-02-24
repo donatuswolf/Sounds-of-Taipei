3003020c68e4bf7e6b9ac27c0f9a19c6a459c071
Mona Ruppert <mona@Mona-Laptop.local>
2019-02-24 13:36:12 +0800

    Update script.js

--- scripts.js
+++ scripts.js
@@ -32,6 +32,7 @@ window.onload = function () {
 
         for (var i = 0; i < 13; i++) {
             drawPlaces(json[i]);
+            console.log(json[i].id);
         }
 
         // for (var i in places) {
