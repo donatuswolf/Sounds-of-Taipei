c96009bd66b32112afe4e3c6ffa854590ea5da9b
Donatus Wolf <doni.wolf@googlemail.com>
2019-02-25 19:12:31 +0800

    comments in script

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 window.onload = function () {
 
-    var mymap = L.map('mapid').setView([25.032, 121.53], 13);
     var radiusMin = 10;
     var radiusMax = 20;
 
@@ -10,7 +9,16 @@ window.onload = function () {
         event: '#ae65d5'
     }
 
+    //// load map ////////
+
+    var lat = 25.032;
+    var lng = 121.53;
+    var zoom = 13;
+
+    var mymap = L.map('mapid').setView([lat, lng], zoom);
     L.tileLayer('https://api.mapbox.com/styles/v1/donatuswolf/cjshijl1c13o41empmevvh85j/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZG9uYXR1c3dvbGYiLCJhIjoiY2pzaGdrcGMzMG40aDQzbjVudTJhZzZ6ZyJ9.McGYoRXAEFRlb9lG8CMXmg').addTo(mymap);
+
+    //// load dataset from json ////////
 
     var places = $.getJSON("places.json", function (json) {
         console.log(json); // show the JSON file content into console
@@ -19,42 +27,49 @@ window.onload = function () {
             drawPlaces(json[i]);
         }
 
+        //// create circles ////////
+
         function drawPlaces(data) {
             var circle = L.circleMarker([data.x, data.y], {
                 // color: data.type,
-                color: color[data.type],
+                color: color[data.type], // color cirlces with type-colors
                 // fillColor: '#f03',
                 fillOpacity: 1,
-                radius: radiusMin
+                radius: radiusMin // default size of cirlces
             }).addTo(mymap);
 
+            //// mouseover ////////
+
             circle.on('mouseover', function () {
-                // this.setStyle({ color: 'blue' });
                 playSound(data.id, data.vol);
                 // this.setRadius(radiusMax)
-                this.setRadius(map(data.db, 0, 1, radiusMin, radiusMax));
-                // $('#photo').attr('background', 'url(assets/photos/' + data.id + '.png)');
-                $('#photo').css('background-image', 'url(assets/photos/' + data.id + '.png)');
-                $('#label').html(data.label);
-                $('#photo').removeClass('hidden');
+                this.setRadius(map(data.db, 0, 1, radiusMin, radiusMax)); // size of cirlces when hovered
+
+                $('#photo').css('background-image', 'url(assets/photos/' + data.id + '.png)'); // load photo
+                $('#label').html(data.label); // add text
+                $('#photo').removeClass('hidden'); // show photo
             });
+
+            //// doubleclick ////////
 
             circle.on('dblclick', function () {
                 if (data.googlemaps != '') {
-                    window.open(data.googlemaps, '_blank');
+                    window.open(data.googlemaps, '_blank'); // create link
                 }
             });
 
             function playSound(name, volume) {
-                var audio = new Audio('assets/sounds/' + name + '.mp3');
-                audio.volume = volume;
+                var audio = new Audio('assets/sounds/' + name + '.mp3'); // load audio
+                audio.volume = volume; // set to custom volume from dataset
                 audio.loop = true;
                 audio.play();
+
+                //// mouseout ////////
 
                 circle.on('mouseout', function () {
                     audio.pause();
                     circle.setRadius(radiusMin)
-                    $('#photo').addClass('hidden');
+                    $('#photo').addClass('hidden'); // hide photo
                 });
             }
         }
