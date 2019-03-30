c43bc9f407739980dbddd79faf3028c12c6eb792
Patrick Schneider <padschneider@gmail.com>
2019-03-30 18:08:33 +0800

    rotating chevron

--- toggleSidebar.js
+++ toggleSidebar.js
@@ -4,21 +4,27 @@ let toggleSidebarStatus = true;
         let getSidebar = document.querySelector('.sidebar');
         let getCenter = document.querySelector('.center');
         let getBottom = document.querySelector('.bottom');
+        let setArrow = document.querySelector('.more');
 
         if (toggleSidebarStatus === false) {
             getSidebar.style.height = '416px';
+            setArrow.style.transform = 'rotate(0deg)';
 
             getCenter.style.opacity = 1;
             getBottom.style.opacity = 1;
 
             toggleSidebarStatus = true;
 
+            
+
         } else if (toggleSidebarStatus === true) {
             getCenter.style.opacity = 0;
             getBottom.style.opacity = 0;
 
             getSidebar.style.height = '104px';
+            setArrow.style.transform = 'rotate(180deg)';
 
             toggleSidebarStatus = false;
+
         }
     }
